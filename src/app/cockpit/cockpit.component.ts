d47f5d5cbb742a2d37c0532f11db396ecc170823
Helen <heb264@gmail.com>
2017-12-12 12:27:53 +0800

    added element hooks

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output,ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -9,19 +9,22 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = "";
-  newServerContent = "";
+  //newServerName = "";
+  //newServerContent = "";
+ @ViewChild('serverContentInput') serverContentInput: ElementRef;
   constructor() {}
 
   ngOnInit() {}
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, 
-      serverContent: this.newServerContent});
+  onAddServer(nameInput) {
+    this.serverCreated.emit({serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value});
   }
 
-  onAddBlueprint() {
-    this.serverCreated.emit({serverName: this.newServerName, 
-      serverContent: this.newServerContent});
+  onAddBlueprint(nameInput) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
   }
 }
